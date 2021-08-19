3549ad2fd4d777266a8ddbe206ce36920c5f720f
Ninja <s223012@studenti.polito.it>
2021-08-19 21:40:42 +0200

    update comments

--- tokenizers/test_class/example.py
+++ tokenizers/test_class/example.py
@@ -1,127 +1,308 @@
 d1 = {
     "0": [
-        "@ dev multiplies two numbers , throws on overflow ."
+        "@ dev returns the addition of two unsigned integers , reverting on overflow . counterpart to solidity ' s + operator . requirements : addition cannot overflow ."
     ],
     "1": [
-        "@ dev integer division of two numbers , truncating the quotient . assert ( b > 0 ) ; solidity automatically throws when dividing by 0 assert ( a b"
+        "@ dev returns the subtraction of two unsigned integers , reverting on overflow ( when the result is negative ) . counterpart to solidity ' s operator . requirements :"
     ],
     "2": [
-        "@ dev subtracts two numbers , throws on overflow ( i . e . if subtrahend is greater than minuend ) ."
+        "@ dev returns the subtraction of two unsigned integers , reverting with custom message on overflow ( when the result is negative ) . counterpart to solidity ' s operator"
     ],
     "3": [
-        "@ dev adds two numbers , throws on overflow ."
+        "@ dev returns the multiplication of two unsigned integers , reverting on overflow . counterpart to solidity ' s operator . requirements : multiplication cannot overflow . gas optimization :"
     ],
     "4": [
-        "@ param owner the address from which the balance will be retrieved @ return the balance"
+        "@ dev returns the integer division of two unsigned integers . reverts on division by zero . the result is rounded towards zero . counterpart to solidity ' s operator"
     ],
     "5": [
-        "send value amount of tokens to address to"
+        "@ dev returns the integer division of two unsigned integers . reverts with custom message on division by zero . the result is rounded towards zero . counterpart to solidity"
     ],
     "6": [
-        "get the address to query ."
+        "@ dev returns the remainder of dividing two unsigned integers . ( unsigned integer modulo ) , reverts when dividing by zero . counterpart to solidity ' s % operator"
     ],
     "7": [
-        "send value amount of tokens to address to"
+        "@ dev returns the remainder of dividing two unsigned integers . ( unsigned integer modulo ) , reverts with custom message when dividing by zero . counterpart to solidity '"
     ],
     "8": [
-        "@ dev emitted when value tokens are moved from one account ( from ) to another ( to ) . note that value may be zero ."
+        "@ dev returns the amount of tokens in existence ."
     ],
     "9": [
-        "@ param owner the address of the account owning tokens @ param spender the address of the account able to transfer the tokens @ return amount of constant allowed value"
+        "@ dev returns the amount of tokens owned by account ."
     ],
     "10": [
-        "@ dev transfer tokens from one address to another . @ param from the address you want to send tokens from . @ param to the address you want to"
+        "@ dev moves amount tokens from the caller ' s account to recipient . returns a boolean value indicating whether the operation succeeded . emits a { transfer } event"
     ],
     "11": [
-        "set allowance for other address allows spender to spend no more than value tokens on your behalf @ param spender the address authorized to spend @ param value the max"
+        "@ dev returns the remaining number of tokens that spender will be allowed to spend on behalf of owner through { transfer from } . this is zero by default"
     ],
     "12": [
-        "@ dev emitted when the allowance of a spender for an owner is set by a call to { approve } . value is the new allowance ."
+        "@ dev sets amount as the allowance of spender over the caller ' s tokens . returns a boolean value indicating whether the operation succeeded . important : beware that"
     ],
     "13": [
-        "@ dev distr token transfer @ param to the address of the sender @ param value the amount of token to be transferred @ return true if the given value"
-    ],
-    "14": [
-        "@ return total amount of tokens"
-    ],
-    "15": [
-        "@ param owner the address from which the balance will be retrieved @ return the balance"
-    ],
-    "16": [
-        "@ dev emitted when value tokens are moved from one account ( from ) to another ( to ) . note that value may be zero ."
-    ],
-    "17": [
-        "@ dev emitted when the allowance of a spender for an owner is set by a call to { approve } . value is the new allowance ."
-    ],
-    "18": [
-        "events"
-    ],
-    "19": [
-        "@ dev emitted during finish minting"
-    ],
-    "20": [
-        "@ dev emitted when value tokens are burnt from one account ( burner ) @ param burner address which burned tokens @ param value amount of tokens burned"
-    ],
-    "21": [
-        "@ dev tokens can be minted only before minting finished ."
-    ],
-    "22": [
-        "@ dev throws if called by any account other than the owner ."
-    ],
-    "23": [
-        "@ dev throws if called by any account other than the blacklist"
-    ],
-    "24": [
-        "@ dev gets the balance of database"
-    ],
-    "25": [
-        "@ dev allows the current owner to transfer control of the contract to a new owner . @ param new owner the address to transfer ownership to ."
-    ],
-    "26": [
-        "@ notice distribution tokens and nothing the function"
-    ],
-    "27": [
-        "@ dev private function to distr alpaca"
-    ],
-    "28": [
-        "@ dev fallback function to accept eth . goblins will send eth back the pool ."
-    ],
-    "29": [
-        "@ dev get investor staking contract ."
-    ],
-    "30": [
-        "@ dev gets the balance of the specified address . @ param owner the address to query the the balance of . @ return an uint256 representing the amount owned"
-    ],
-    "31": [
-        "mitigates the erc20 short address attack"
-    ],
-    "32": [
-        "@ dev transfer token for a specified address @ param to the address to transfer to . @ param amount the amount to be transferred ."
-    ],
-    "33": [
         "@ dev moves amount tokens from sender to recipient using the allowance mechanism . amount is then deducted from the caller ' s allowance . returns a boolean value indicating"
     ],
+    "14": [
+        "@ dev emitted when value tokens are moved from one account ( from ) to another ( to ) . note that value may be zero ."
+    ],
+    "15": [
+        "@ dev emitted when the allowance of a spender for an owner is set by a call to { approve } . value is the new allowance ."
+    ],
+    "16": [
+        "events"
+    ],
+    "17": [
+        "events"
+    ],
+    "18": [
+        "@ dev returns the address of the current owner ."
+    ],
+    "19": [
+        "@ notice return owner address"
+    ],
+    "20": [
+        "@ notice set a new owner address"
+    ],
+    "21": [
+        "@ notice allows the owner to authorized owner @ param assume owner address of the new owner"
+    ],
+    "22": [
+        "@ dev sets the implementer contract as addr for interface hash for interface hash . addr is an { ierc165 } event . if interface hash is an { ierc165"
+    ],
+    "23": [
+        "@ dev returns the implementer of interface hash for addr . if no such implementer is registered , ) @ param addr the address of the contract @ param interface"
+    ],
+    "24": [
+        "@ dev sets new manager as the manager for new manager . @ dev governance function called by the delegator manager @ param addr is the address of the new"
+    ],
+    "25": [
+        "@ dev get the given address for a manager @ param addr is the address of the member @ return addr is the manager address"
+    ],
+    "26": [
+        "@ dev sets the implementation address of the proxy . @ param Label the address of the new implementation . @ param implementation the address of the contract to be"
+    ],
+    "27": [
+        "@ dev contract implements the address of the interface @ param addr address of the contract to query @ param interface id id of the contract"
+    ],
+    "28": [
+        "@ dev delegate manager address to a manager @ param new ERC1820REGISTRY the new ERC1820REGISTRY manager address"
+    ],
+    "29": [
+        "@ dev returns the implementer # interface } ."
+    ],
+    "30": [
+        "@ dev sets the interface for { ierc165 } itself , as recovered"
+    ],
+    "31": [
+        "@ dev know the reason on success or failure based on the eip 1066 application specific status codes . @ param function sig id of the function that needs to"
+    ],
+    "32": [
+        "@ dev called by an { ierc777 } when a need ."
+    ],
+    "33": [
+        "@ dev know the reason on success or failure based on the eip 1066 application specific status codes . @ param function sig id of the function that needs to"
+    ],
     "34": [
-        "allow spender to withdraw from your account , multiple times . to change the approve amount you first have to reduce the addresses allowance to zero by calling approve ("
+        "@ dev called by an { ierc777 } token contract whenever operator roles library contracts of a given partition . @ param function name of the function that needs to"
     ],
     "35": [
-        "@ dev function to check the amount of tokens that an owner allowed to a spender . @ param owner address the address which owns the funds . @ param"
+        "@ dev called by an { ierc777 } token contract whenever operator to be approved for all or the other side . @ param function name of the function that"
     ],
     "36": [
-        "@ param token address the address of the token contract @ return the token address"
+        "@ dev called by an { ierc777 } token contract whenever operator address ."
     ],
     "37": [
-        "@ dev withdraw ether from contract @ param ether address uint256 amount of ether transfer"
+        "@ dev returns true if an account is an operator of token holder . operators can send and burn tokens on behalf of their owners . all accounts are their"
     ],
     "38": [
-        "@ dev burns a specific amount of tokens . @ param value the amount of token to be burned . no need to require value < total supply , since"
+        "@ notice get the built on frontend ."
     ],
     "39": [
-        "@ dev withdraw ether to withdraw tokens from contract"
+        "@ dev get reserved encoding for a specific partition . @ param partition name of the partition ."
+    ],
+    "40": [
+        "@ dev get collateral occupied value . @ param partition name of the partition ."
+    ],
+    "41": [
+        "@ notice get the address of the given prefix ' s address"
+    ],
+    "42": [
+        "@ dev returns the remaining number of tokens that spender will be allowed to spend on behalf of owner through { transfer from } . this is zero by default"
+    ],
+    "43": [
+        "@ notice send value token to to from from on the condition it is approved by from @ param from the address of the sender @ param to the address"
+    ],
+    "44": [
+        "@ dev emitted when value tokens of token type from from to to ."
+    ],
+    "45": [
+        "@ dev emitted when an option contract changes"
+    ],
+    "46": [
+        "@ dev emitted when the allowance of a spender for an owner is set by a call to { approve } . value is the new allowance ."
+    ],
+    "47": [
+        "@ dev emitted when an account operator is granted operator of an operator is allowed to transfer all tokens of the sender on their behalf ."
+    ],
+    "48": [
+        "@ dev emitted when account is revoked token type token is transferred ."
+    ],
+    "49": [
+        "@ dev emitted when operator for a specific partition ."
     ]
 }
-d2 = {}
-file_path = "smart_contracts/SiaCashCoin.comment"
+d2 = {
+    "0": [
+        "@ dev emitted when token holder"
+    ],
+    "1": [
+        "@ notice emitted when a collateral manager is changed"
+    ],
+    "2": [
+        "event that tracks default extension contract address change"
+    ],
+    "3": [
+        "erc223"
+    ],
+    "4": [
+        "@ dev emitted when value tokens are moved from one account ( operator ) to another ( to ) . note that value may be zero ."
+    ],
+    "5": [
+        "@ dev get total supply of token"
+    ],
+    "6": [
+        "get the token balance for account token owner"
+    ],
+    "7": [
+        "@ dev transfer tokens to a specified address . @ param to the address to transfer to . @ param value the amount to be transferred . @ return true"
+    ],
+    "8": [
+        "@ dev transfer tokens from one address to another . @ param from the address which you want to send tokens from @ param to the address which you want"
+    ],
+    "9": [
+        "@ dev returns the remaining number of tokens that spender will be allowed to spend on behalf of owner through { transfer from } . this is zero by default"
+    ],
+    "10": [
+        "@ dev approve the passed address to spend the specified amount of tokens on behalf of msg . sender . @ param spender the address which will spend the funds"
+    ],
+    "11": [
+        "@ dev atomically increases the allowance granted to spender by the caller . this is an alternative to { approve } that can be used as a mitigation for problems"
+    ],
+    "12": [
+        "@ dev atomically decreases the allowance granted to spender by the caller . this is an alternative to { approve } that can be used as a mitigation for problems"
+    ],
+    "13": [
+        "@ dev uniswap fails with zero address so no check is necessary here"
+    ],
+    "14": [
+        "@ dev get chainlink"
+    ],
+    "15": [
+        "@ dev returns the partitions data ."
+    ],
+    "16": [
+        "@ dev transfer tokens from a specific partition . @ param partition name of the partition . @ param from the address which you want to send tokens . @"
+    ],
+    "17": [
+        "@ dev see { ierc777 authorize operator } . emits an { approval } event indicating the updated allowance ."
+    ],
+    "18": [
+        "@ dev see { ierc777 revoke operator } ."
+    ],
+    "19": [
+        "@ dev authorize tokens for a specific partition . @ param partition name of the partition . @ param operator address of the operator . @ param operator address of"
+    ],
+    "20": [
+        "@ dev allows ' operator ' as an operator for ' msg . sender ' for a given partition . @ param partition name of the partition . @ param"
+    ],
+    "21": [
+        "is operator ( ) : returns true if operator is allowed to transfer ownership of token holder"
+    ],
+    "22": [
+        "@ dev returns whether an operator is a specific partition . @ param partition name of the partition . @ param operator address which may be an operator of token"
+    ],
+    "23": [
+        "@ dev checks whether partition is a specific partition is available for a given partition . @ param operator address for partition . @ param operator address for operator address"
+    ],
+    "24": [
+        "@ dev returns the name of the token . @ return string representing the token name"
+    ],
+    "25": [
+        "@ dev gets the token symbol . @ return string representing the token symbol"
+    ],
+    "26": [
+        "@ dev allows users to access the number of decimals"
+    ],
+    "27": [
+        "@ dev see { ierc777 granularity } . this implementation always returns granularity ."
+    ],
+    "28": [
+        "@ dev returns the total quantity"
+    ],
+    "29": [
+        "@ dev check the value of tokens that an owner allowed to a spender . @ param partition name of the partition . @ param owner address the address which"
+    ],
+    "30": [
+        "@ dev approve the passed address to spend the specified amount of tokens on behalf of msg . sender . @ param partition name of the partition . @ param"
+    ],
+    "31": [
+        "@ dev increase the amount of tokens that an owner allowed to a spender . @ param partition name of the partition . @ param spender the address which is"
+    ],
+    "32": [
+        "@ dev decrease the amount of tokens that an owner allowed to a spender . approve should be called when allowed [ msg . sender ] , but also ."
+    ],
+    "33": [
+        "@ notice registers collateral manager ."
+    ],
+    "34": [
+        "@ dev checks if a manager is a manager ."
+    ],
+    "35": [
+        "@ dev updates the implementation of the given implementation @ param prefix id the address of the new implementation . @ param implementation the address of the new implementation ."
+    ],
+    "36": [
+        "@ notice returns true if the given address is currently active @ param prefix"
+    ],
+    "37": [
+        "@ dev transfer tokens from one address to another . @ param from the address you want to send tokens from . @ param to the address you want to"
+    ],
+    "38": [
+        "@ dev transfer tokens from one address to another . @ param operator address which you want to send tokens from . @ param to the address which you want"
+    ],
+    "39": [
+        "@ dev internal function that determines for a specific partition ."
+    ],
+    "40": [
+        "@ dev add a specific partition for a specific partition . @ param to address of the to check . @ param value the amount of tokens to be added"
+    ],
+    "41": [
+        "@ dev adds a new total partition . @ param partition name of the partition ."
+    ],
+    "42": [
+        "@ dev remove value of a specific partition . @ param partition name of the partition ."
+    ],
+    "43": [
+        "@ dev handles the contract extends a specific months due to the given operator . @ param from the address of the sender . @ param operator address of the"
+    ],
+    "44": [
+        "@ dev call through ds of a specific partition . @ param to the address of the partition . @ param operator address of the operator for individual address of"
+    ],
+    "45": [
+        "@ dev sets amount as the allowance of spender over the partition parameter . @ param partition name of the partition . @ param token address for the address that"
+    ],
+    "46": [
+        "is operator ( ) : returns true if operator is allowed to transfer ownership of token holder"
+    ],
+    "47": [
+        "@ dev returns whether the operator is allowed to manage all of the token holder . @ param partition name of the partition . @ param token address for which"
+    ],
+    "48": [
+        "@ notice calls the logic contract ' s name @ param partition name of the partition . @ param operator address of the token holder . @ param token holder"
+    ],
+    "49": [
+        "events"
+    ]
+}
+file_path = "smart_contracts/AMNToken.comment"
 
 
 def merge_dict(d1, d2, file_path):
